db7a70d8bd3de699884909cba155322f4a6e2755
Tommy Seo <tommyseo7m@gmail.com>
2023-11-19 21:58:31 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,44 +1,50 @@
-# WEB102 Prework - *Name of App Here*
+# WEB102 Prework - X6
 
-Submitted by: **Your Name Here**
+Submitted by: Dong Hyun Seo
 
-**Name of your app** is a website for the company Sea Monster Crowdfunding that displays information about the games they have funded.
+X6 is a website for the company Sea Monster Crowdfunding that displays information about the games they have funded.
 
-Time spent: **X** hours spent in total
+Time spent: 6 hours spent in total
 
 ## Required Features
 
 The following **required** functionality is completed:
 
-* [ ] The introduction section explains the background of the company and how many games remain unfunded.
-* [ ] The Stats section includes information about the total contributions and dollars raised as well as the top two most funded games.
-* [ ] The Our Games section initially displays all games funded by Sea Monster Crowdfunding
-* [ ] The Our Games section has three buttons that allow the user to display only unfunded games, only funded games, or all games.
+* [X] The introduction section explains the background of the company and how many games remain unfunded.
+* [X] The Stats section includes information about the total contributions and dollars raised as well as the top two most funded games.
+* [X] The Our Games section initially displays all games funded by Sea Monster Crowdfunding
+* [X] The Our Games section has three buttons that allow the user to display only unfunded games, only funded games, or all games.
 
 The following **optional** features are implemented:
+Adjusted color schema so it is high contrast and relevant to the organization's theme color
+Increased font size and added hover glow effect for all 3 buttons within the Our Games section
+Custom Feature: Hide email and phone contact information and let it be toggled with buttons: Great for saving webpage space and only showing information upon user's request.
 
-* [ ] List anything else that you can get done to improve the app functionality!
-
+* [X] List anything else that you can get done to improve the app functionality!
+I can incorporate a search bar within the Our Games section to allow users to search for specific games they want to find
+I can program the cards to be in collapsed format and allow the user to expand them to see the game details such as the description, pledged amount, goal, and backers 
+in order to save webpage space in case Sea Monster scales up and more games are added to the library.
+I can store cookies so that the site remembers where the user left off on the site.
 ## Video Walkthrough
 
 Here's a walkthrough of implemented features:
 
-<img src='http://i.imgur.com/link/to/your/gif/file.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+<img src="https://i.imgur.com/K9nCthQ.gif" title="source: imgur.com" />
 
 <!-- Replace this with whatever GIF tool you used! -->
 GIF created with ...  
-<!-- Recommended tools:
-[Kap](https://getkap.co/) for macOS
-[ScreenToGif](https://www.screentogif.com/) for Windows
-[peek](https://github.com/phw/peek) for Linux. -->
+[LiceCap] https://www.cockos.com/licecap/ for MacOS
 
 ## Notes
 
 Describe any challenges encountered while building the app.
 
+I had to turn to the notes on the codepath site to implement the use destructuring and the spread operator to grab the first and second games.
+It took some time to understand the pattern and logic of destructuring and the spread operator, but because I had prior Python knowledge, it helped me understand.
+
 ## License
 
-    Copyright [yyyy] [name of copyright owner]
+    Copyright 2023 Dong Hyun Seo
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
--- index.js
+++ index.js
@@ -29,27 +29,33 @@ const gamesContainer = document.getElementById("games-container");
 function addGamesToPage(games) {
 
     // loop over each item in the data
-
+    games.forEach(function(game) {
 
         // create a new div element, which will become the game card
-
+        let gameCard = document.createElement('div');
 
         // add the class game-card to the list
-
+        gameCard.classList.add('game-card');
 
         // set the inner HTML using a template literal to display some info 
         // about each game
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
-
-
+        gameCard.innerHTML = `
+            <img src="${game.img}" class="game-img" alt="${game.name}">
+            <h3>${game.name}</h3>
+            <p>${game.description}</p>
+            <div>Pledged: $${game.pledged.toLocaleString()} / $${game.goal.toLocaleString()}</div>
+            <div>Backers: ${game.backers}</div>
+        `;
         // append the game to the games-container
-
+        gamesContainer.appendChild(gameCard);
+    });
 }
 
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
-
+addGamesToPage(GAMES_JSON);
 
 /*************************************************************************************
  * Challenge 4: Create the summary statistics at the top of the page displaying the
@@ -61,20 +67,31 @@ function addGamesToPage(games) {
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
-
+const totalContributions = GAMES_JSON.reduce((accumulator, game) => {
+    return accumulator + game.backers;
+}, 0);
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
-
+contributionsCard.innerHTML = `${totalContributions.toLocaleString()}`;
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
 
-// set inner HTML using template literal
+const totalPledged = GAMES_JSON.reduce((accumulator, game) => {
+    return accumulator + game.pledged;
+}, 0);
 
+// set inner HTML using template literal
+raisedCard.innerHTML = `$${totalPledged.toLocaleString()}`;
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
 
+const totalGames = GAMES_JSON.reduce((accumulator, game) => {
+    return accumulator + 1;
+}, 0);
+
+gamesCard.innerHTML = `${totalGames.toLocaleString()}`;
 
 /*************************************************************************************
  * Challenge 5: Add functions to filter the funded and unfunded games
@@ -87,10 +104,11 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
-
+    const unFundedGames = GAMES_JSON.filter(game => game.pledged < game.goal);
+    console.log(unFundedGames.length);
 
     // use the function we previously created to add the unfunded games to the DOM
-
+    addGamesToPage(unFundedGames);
 }
 
 // show only games that are fully funded
@@ -98,10 +116,11 @@ function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
-
+    const fundedGames = GAMES_JSON.filter(game => game.pledged >= game.goal);
+    console.log(fundedGames.length);
 
     // use the function we previously created to add unfunded games to the DOM
-
+    addGamesToPage(fundedGames);
 }
 
 // show all games
@@ -109,6 +128,7 @@ function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
+    addGamesToPage(GAMES_JSON);
 
 }
 
@@ -118,7 +138,9 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
-
+document.getElementById("unfunded-btn").addEventListener("click", filterUnfundedOnly);
+document.getElementById("funded-btn").addEventListener("click", filterFundedOnly);
+document.getElementById("all-btn").addEventListener("click", showAllGames);
 
 /*************************************************************************************
  * Challenge 6: Add more information at the top of the page about the company.
@@ -129,12 +151,19 @@ const allBtn = document.getElementById("all-btn");
 const descriptionContainer = document.getElementById("description-container");
 
 // use filter or reduce to count the number of unfunded games
-
+const totalUnfundedGames = GAMES_JSON.reduce((acc, game) => {
+    return game.pledged < game.goal ? acc + 1 : acc;
+}, 0);
 
 // create a string that explains the number of unfunded games using the ternary operator
-
+const displayStr = totalUnfundedGames <= 1 
+? `A total of ${totalPledged.toLocaleString()} have been raised for ${totalGames} games. Currently ${totalUnfundedGames} remains unfunded. We need your help to fund these amazing games!` 
+: `A total of ${totalPledged.toLocaleString()} have been raised for ${totalGames} games. Currently ${totalUnfundedGames} remain unfunded. We need your help to fund these amazing games!`
 
 // create a new DOM element containing the template string and append it to the description container
+const infoElement = document.createElement("p");
+infoElement.innerHTML = displayStr;
+descriptionContainer.appendChild(infoElement);
 
 /************************************************************************************
  * Challenge 7: Select & display the top 2 games
@@ -149,7 +178,46 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+const [firstGame, secondGame, ...rest] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const topPledgedElement = document.createElement("h2");
+topPledgedElement.innerHTML = firstGame.name;
+firstGameContainer.appendChild(topPledgedElement);
 
 // do the same for the runner up item
+const runnerUpPledgedElement = document.createElement("h2");
+runnerUpPledgedElement.innerHTML = secondGame.name;
+secondGameContainer.appendChild(runnerUpPledgedElement);
+
+/*************************************************************************************
+ * Custom Feature: Hide email and phone contact information and let it be toggled with buttons:
+ * Great for saving webpage space and only showing information upon user's request.
+*/
+
+
+// Select the button and the message paragraph
+const toggleMessageBtn1 = document.getElementById("toggle-message-btn-1");
+const specialMessage1 = document.getElementById("special-message-1");
+const toggleMessageBtn2 = document.getElementById("toggle-message-btn-2");
+const specialMessage2 = document.getElementById("special-message-2");
+
+// Add event listener to the email button
+toggleMessageBtn1.addEventListener("click", function() {
+    // Toggle the display of the message
+    if (specialMessage1.style.display === "none") {
+        specialMessage1.style.display = "block";
+    } else {
+        specialMessage1.style.display = "none";
+    }
+});
+
+// Add event listener to the phone button
+toggleMessageBtn2.addEventListener("click", function() {
+    // Toggle the display of the message
+    if (specialMessage2.style.display === "none") {
+        specialMessage2.style.display = "block";
+    } else {
+        specialMessage2.style.display = "none";
+    }
+});
